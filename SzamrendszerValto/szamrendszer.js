6f2242cbd519ae24d41de2e49f4390f72a053ebf
Nyámándi Sebestyén <152324833+NyamandiSebestyen@users.noreply.github.com>
2024-05-25 16:16:27 +0200

    Add files via upload

--- SzamrendszerValto/szamrendszer.js
+++ SzamrendszerValto/szamrendszer.js
@@ -1,8 +1,15 @@
 function barmibolTizesbe(szam, szamRendszer) {
     var szamTomb = szam.split("");
+    if (szamTomb.length == 0) {
+        return -1;
+    }
+    ;
     var szumma = 0;
     for (var i = 0; i < szamTomb.length; i++) {
-        if (Number(szamTomb[i]) > szamRendszer - 1) {
+        if (isNaN(Number(szamTomb[i]))) {
+            szamTomb[i] = betuSzamok[szamTomb[i].toLowerCase()];
+        }
+        if (Number(szamTomb[i]) > szamRendszer - 1 || szamTomb[i] == undefined) {
             return -1;
         }
         szumma += Number(szamTomb[i]) * (Math.pow(szamRendszer, (szamTomb.length - 1 - i)));
@@ -15,7 +22,12 @@ function tizbolBarmibe(bemenetiSzam, szamRendszerbe) {
     }
     var eredmenySzam = "";
     while (bemenetiSzam > 0) {
-        eredmenySzam += (bemenetiSzam % szamRendszerbe).toString();
+        if (bemenetiSzam % szamRendszerbe < 10) {
+            eredmenySzam += (bemenetiSzam % szamRendszerbe).toString();
+        }
+        else {
+            eredmenySzam += szamBetuk[bemenetiSzam % szamRendszerbe];
+        }
         bemenetiSzam = Math.floor(bemenetiSzam / szamRendszerbe);
     }
     return eredmenySzam.split("").reverse().join("");
@@ -31,8 +43,24 @@ function futtato() {
     var kimenetiSzamR = document.querySelector("#kimenetiSzamrendszer");
     var kimenetiSzamRErtek = parseInt(kimenetiSzamR.value);
     var eredmeny = document.querySelector("#eredmeny");
-    (BarmibolBarmibeValto(bemenetiErtek, bemenetiSzamRErtek, kimenetiSzamRErtek) == "-1") ? eredmeny.innerHTML = "Rossz bemenet!" : eredmeny.innerHTML = "".concat(bemenetiErtek, "<sub>").concat(bemenetiSzamRErtek, "</sub> = ").concat(BarmibolBarmibeValto(bemenetiErtek, bemenetiSzamRErtek, kimenetiSzamRErtek), "<sub>").concat(kimenetiSzamRErtek, "</sub>");
+    (BarmibolBarmibeValto(bemenetiErtek, bemenetiSzamRErtek, kimenetiSzamRErtek) == "-1") ? eredmeny.innerHTML = "Rossz bemenet!" : eredmeny.innerHTML = "".concat(bemenetiErtek.toUpperCase(), "<sub>").concat(bemenetiSzamRErtek, "</sub> = ").concat(BarmibolBarmibeValto(bemenetiErtek, bemenetiSzamRErtek, kimenetiSzamRErtek).toUpperCase(), "<sub>").concat(kimenetiSzamRErtek, "</sub>");
     eredmeny.style.border = "4px solid black";
 }
+var betuSzamok = {
+    a: "10",
+    b: "11",
+    c: "12",
+    d: "13",
+    e: "14",
+    f: "15"
+};
+var szamBetuk = {
+    10: "a",
+    11: "b",
+    12: "c",
+    13: "d",
+    14: "e",
+    15: "f"
+};
 var gomb = document.querySelector("#valtoGomb");
 gomb.addEventListener("click", futtato);
--- SzamrendszerValto/szamrendszer.ts
+++ SzamrendszerValto/szamrendszer.ts
@@ -1,10 +1,16 @@
 function barmibolTizesbe(szam:string,szamRendszer:number):number{
     let szamTomb:string[]=szam.split("");
+    if(szamTomb.length == 0){return -1;};
     let szumma:number = 0;
     for(let i:number=0;i<szamTomb.length;i++){
-        if(Number(szamTomb[i])>szamRendszer-1){return -1;}
+        if(isNaN(Number(szamTomb[i]))){
+            szamTomb[i]=betuSzamok[szamTomb[i].toLowerCase()];
+        }
+        
+        if(Number(szamTomb[i])>szamRendszer-1 || szamTomb[i]== undefined ){return -1;}
        szumma+=Number(szamTomb[i])*(szamRendszer**(szamTomb.length-1-i));
-    }
+        }
+    
     return szumma;
 }
 
@@ -12,7 +18,12 @@ function tizbolBarmibe(bemenetiSzam:number,szamRendszerbe:number):string{
     if(bemenetiSzam==-1){return "-1";}
     let eredmenySzam:string="";
    while(bemenetiSzam>0){
-    eredmenySzam+=(bemenetiSzam%szamRendszerbe).toString();
+    if(bemenetiSzam%szamRendszerbe<10){
+        eredmenySzam+=(bemenetiSzam%szamRendszerbe).toString();
+    }
+    else{
+        eredmenySzam+=szamBetuk[bemenetiSzam%szamRendszerbe];
+    }
     bemenetiSzam = Math.floor(bemenetiSzam / szamRendszerbe);
    }
    return eredmenySzam.split("").reverse().join("");
@@ -30,9 +41,28 @@ function futtato():void{
     let kimenetiSzamR:HTMLSelectElement = document.querySelector("#kimenetiSzamrendszer");
     let kimenetiSzamRErtek:number = parseInt(kimenetiSzamR.value);
     let eredmeny:HTMLParagraphElement = document.querySelector("#eredmeny");
-    (BarmibolBarmibeValto(bemenetiErtek,bemenetiSzamRErtek,kimenetiSzamRErtek)=="-1")?eredmeny.innerHTML="Rossz bemenet!":eredmeny.innerHTML=`${bemenetiErtek}<sub>${bemenetiSzamRErtek}</sub> = ${BarmibolBarmibeValto(bemenetiErtek,bemenetiSzamRErtek,kimenetiSzamRErtek)}<sub>${kimenetiSzamRErtek}</sub>`;
+    (BarmibolBarmibeValto(bemenetiErtek,bemenetiSzamRErtek,kimenetiSzamRErtek)=="-1")?eredmeny.innerHTML="Rossz bemenet!":eredmeny.innerHTML=`${bemenetiErtek.toUpperCase()}<sub>${bemenetiSzamRErtek}</sub> = ${BarmibolBarmibeValto(bemenetiErtek,bemenetiSzamRErtek,kimenetiSzamRErtek).toUpperCase()}<sub>${kimenetiSzamRErtek}</sub>`;
     eredmeny.style.border = "4px solid black";
 }
+
+
+const betuSzamok: {[betuk:string]:string} = {
+    a: "10",
+    b: "11",
+    c: "12",
+    d: "13",
+    e: "14",
+    f: "15"
+};
+
+const szamBetuk: {[szamok:number]:string} = {
+    10: "a",
+    11: "b",
+    12: "c",
+    13: "d",
+    14: "e",
+    15: "f"
+};
 
 let gomb: HTMLButtonElement = document.querySelector("#valtoGomb");
 gomb.addEventListener("click",futtato);
